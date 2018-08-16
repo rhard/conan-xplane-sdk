3efd9abbdfb4619673a5fe18739915357f564aa2
Roman Liubich <roman.liubich@siemens.com>
2018-08-16 13:19:35 +0200

    Filter build

--- build.py
+++ build.py
@@ -3,5 +3,12 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add(settings={"arch": "x86_64"}, options={}, env_vars={}, build_requires={})
+    builder.add_common_builds()
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if settings["build_type"] != "Debug" and settings["arch"] != "x86":
+            if platform.system() == "Windows" and settings["compiler.runtime"] == "MD":
+                continue
+            filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
